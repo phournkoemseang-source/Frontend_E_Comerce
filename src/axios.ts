61c797be1a2b56526938dbd9982648a7965ed8e9
phournkoemseang-source <phournkoemseang@gmail.com>
2026-06-29 00:09:50 +0700

    create file .env

--- src/axios.ts
+++ src/axios.ts
@@ -8,7 +8,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: '/api',
+    // baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
